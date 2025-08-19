ea437808cb43e09fcdf4983c42f7920b9065cfe0
rajeshkanna <rajeshkannathiru10@gmail.com>
2025-08-19 11:28:32 +0530

    Updates

---
+++ precache-manifest.deae0143764d7335f7bb0db1e1f1af4c.js
@@ -0,0 +1,234 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa9560f737b36dbf165ddfad8149c032",
+    "url": "/welfund1/index.html"
+  },
+  {
+    "revision": "df0de11fb8c9e474b1a6",
+    "url": "/welfund1/static/css/2.bdde8d10.chunk.css"
+  },
+  {
+    "revision": "1ffeef10121af15b0533",
+    "url": "/welfund1/static/css/main.6f863db2.chunk.css"
+  },
+  {
+    "revision": "df0de11fb8c9e474b1a6",
+    "url": "/welfund1/static/js/2.808c8251.chunk.js"
+  },
+  {
+    "revision": "c51b4db1899f08b6e515e095255536ed",
+    "url": "/welfund1/static/js/2.808c8251.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ffeef10121af15b0533",
+    "url": "/welfund1/static/js/main.5581c0a2.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/welfund1/static/js/main.5581c0a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bab619f4abd00920333",
+    "url": "/welfund1/static/js/runtime-main.5d0fd5ca.js"
+  },
+  {
+    "revision": "b33407e33dc975025b0ed594aa75e6de",
+    "url": "/welfund1/static/media/11.b33407e3.svg"
+  },
+  {
+    "revision": "d5abfa0e1ca2d81e56adcf2c572e062e",
+    "url": "/welfund1/static/media/2.d5abfa0e.svg"
+  },
+  {
+    "revision": "c87d5b762b6fbba141ea8f7d61d4c3cc",
+    "url": "/welfund1/static/media/22.c87d5b76.svg"
+  },
+  {
+    "revision": "da2f762fd90b2030dff22cd5fbfee6de",
+    "url": "/welfund1/static/media/33.da2f762f.svg"
+  },
+  {
+    "revision": "7b8ac5e4c791a011be98a6e8092a8fed",
+    "url": "/welfund1/static/media/44.7b8ac5e4.svg"
+  },
+  {
+    "revision": "884ec6fc620f89f0c0ac10b7cd2d36c7",
+    "url": "/welfund1/static/media/55.884ec6fc.svg"
+  },
+  {
+    "revision": "8d65c95bbb5d866a24991065c3ab7f69",
+    "url": "/welfund1/static/media/Cover-Page.8d65c95b.png"
+  },
+  {
+    "revision": "4c62644b159530ff6214336f8cfb1d32",
+    "url": "/welfund1/static/media/Futura Bold font.4c62644b.ttf"
+  },
+  {
+    "revision": "f9f02ed05aa86534c3842d44cb20d6c4",
+    "url": "/welfund1/static/media/Futura Book font.f9f02ed0.ttf"
+  },
+  {
+    "revision": "14b3a355f612d6181e891efd2c798b5f",
+    "url": "/welfund1/static/media/Gotham-Black.14b3a355.otf"
+  },
+  {
+    "revision": "9c35bf87f23c8cca614720126fe0baa0",
+    "url": "/welfund1/static/media/Gotham-Bold.9c35bf87.otf"
+  },
+  {
+    "revision": "f76e3adf545b3299f643fd7642800351",
+    "url": "/welfund1/static/media/Gotham-Light.f76e3adf.otf"
+  },
+  {
+    "revision": "b54724f54d4dd3f6796e3c4cc422f998",
+    "url": "/welfund1/static/media/GothamBook.b54724f5.ttf"
+  },
+  {
+    "revision": "77171d8f5b5283f9d47a3434704bf944",
+    "url": "/welfund1/static/media/GothamMedium.77171d8f.ttf"
+  },
+  {
+    "revision": "404c632e3e3c995966dab7b6b1b5775a",
+    "url": "/welfund1/static/media/a.404c632e.svg"
+  },
+  {
+    "revision": "0cf732a0898485e9d9afe3b3dae6400d",
+    "url": "/welfund1/static/media/borrower.0cf732a0.svg"
+  },
+  {
+    "revision": "91c2f2e9e500454d7f4473663fd13bee",
+    "url": "/welfund1/static/media/contact.91c2f2e9.svg"
+  },
+  {
+    "revision": "d4a43de2183174d5c946ba26fd818dad",
+    "url": "/welfund1/static/media/contact1.d4a43de2.svg"
+  },
+  {
+    "revision": "7a771f42b847ca52181491f0790b37ed",
+    "url": "/welfund1/static/media/cost.7a771f42.svg"
+  },
+  {
+    "revision": "7bd7510bb3853f8399fce9bd53fcca4d",
+    "url": "/welfund1/static/media/deals.7bd7510b.svg"
+  },
+  {
+    "revision": "1bc4d288ffe6b55182b196ce02cc6f31",
+    "url": "/welfund1/static/media/faq.1bc4d288.png"
+  },
+  {
+    "revision": "2fc251410d6678627625730907e0701c",
+    "url": "/welfund1/static/media/feedback.2fc25141.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/welfund1/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/welfund1/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/welfund1/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/welfund1/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/welfund1/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "9a1626276b430d216809407c3fbcda75",
+    "url": "/welfund1/static/media/futur.9a162627.ttf"
+  },
+  {
+    "revision": "98d2f97305ab25d4511982e73740922e",
+    "url": "/welfund1/static/media/futura light bt.98d2f973.ttf"
+  },
+  {
+    "revision": "ee64fb9d3f1ba2333e1b489283925bce",
+    "url": "/welfund1/static/media/futura medium bt.ee64fb9d.ttf"
+  },
+  {
+    "revision": "fb5bf6baaeef130ebdc1d15cd0ee76a1",
+    "url": "/welfund1/static/media/get_in_touch.fb5bf6ba.png"
+  },
+  {
+    "revision": "86df02b125bd1449bde2434327fc6132",
+    "url": "/welfund1/static/media/group-1642.86df02b1.png"
+  },
+  {
+    "revision": "15d90ac5282a1d5cbf3724bfc1341933",
+    "url": "/welfund1/static/media/group-2783.15d90ac5.svg"
+  },
+  {
+    "revision": "f2700d1055c83cb552603f1207e1fefd",
+    "url": "/welfund1/static/media/icon-four.f2700d10.svg"
+  },
+  {
+    "revision": "94049fab38093cacab41889a3c3f1027",
+    "url": "/welfund1/static/media/icon-one.94049fab.svg"
+  },
+  {
+    "revision": "8a0f2e4e0c153d4d5373928be48b4168",
+    "url": "/welfund1/static/media/icon-two.8a0f2e4e.svg"
+  },
+  {
+    "revision": "1a537723c817bc92c34c093c2b77af88",
+    "url": "/welfund1/static/media/investor.1a537723.png"
+  },
+  {
+    "revision": "5cdf3b74f253042073d04d2e8f8c81f3",
+    "url": "/welfund1/static/media/logo.5cdf3b74.png"
+  },
+  {
+    "revision": "8b573bbef2d265d4c414dd17d3f76923",
+    "url": "/welfund1/static/media/mail-new.8b573bbe.svg"
+  },
+  {
+    "revision": "9b54e83aa1311b175b978726d438af70",
+    "url": "/welfund1/static/media/mail.9b54e83a.svg"
+  },
+  {
+    "revision": "37ebc6b540b8f64840c5d1878b18577f",
+    "url": "/welfund1/static/media/nnn.37ebc6b5.png"
+  },
+  {
+    "revision": "5aabb282dd7d740c6ec479a8c5ca11bc",
+    "url": "/welfund1/static/media/one.5aabb282.svg"
+  },
+  {
+    "revision": "7235f1533f48b903b5617ac15563f001",
+    "url": "/welfund1/static/media/pie_chart.7235f153.png"
+  },
+  {
+    "revision": "ed0f2543deb65cf50cfeb38b5130a62a",
+    "url": "/welfund1/static/media/risk.ed0f2543.svg"
+  },
+  {
+    "revision": "1640f50b4d4b80deb5f1dcf80e68959f",
+    "url": "/welfund1/static/media/shape2.1640f50b.png"
+  },
+  {
+    "revision": "ff257b6d79b5ada5fdaee0003ba11af0",
+    "url": "/welfund1/static/media/t3.ff257b6d.svg"
+  },
+  {
+    "revision": "50df2a3e35b92ca6681f01f55a02b157",
+    "url": "/welfund1/static/media/thanku.50df2a3e.svg"
+  },
+  {
+    "revision": "0d285ab47cf9271b6bdd5f2a452cacda",
+    "url": "/welfund1/static/media/time.0d285ab4.svg"
+  },
+  {
+    "revision": "4b736dc89162cf759d0c23d409eb3a2c",
+    "url": "/welfund1/static/media/transparency.4b736dc8.svg"
+  },
+  {
+    "revision": "eaecfe05392e5aeebd7fabc7ac4dac48",
+    "url": "/welfund1/static/media/two.eaecfe05.svg"
+  }
+]);
